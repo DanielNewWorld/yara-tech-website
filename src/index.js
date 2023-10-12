170731cd16a8f5aa6677283f90e6ec6747875176
rovshenklichev <kulieyev@gmail.com>
2023-10-12 17:12:39 +0300

    fix git

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import {HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-    <React.StrictMode>
+root.render( <React.StrictMode>
+    <h1>index</h1>
         <HashRouter >
             <Provider store={store}>
                 <App/>
