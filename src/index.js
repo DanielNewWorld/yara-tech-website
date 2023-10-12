f168a4019107b183d4da96d6da80830db4ad738e
rovshenklichev <kulieyev@gmail.com>
2023-10-12 17:31:12 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import {HashRouter} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render( <React.StrictMode>
-    <h1>index</h1>
         <HashRouter >
             <Provider store={store}>
                 <App/>
