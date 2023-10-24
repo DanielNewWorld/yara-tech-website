2225ba49f7d23daea42d479b5d3209bf204496ef
rovshenklichev <kulieyev@gmail.com>
2023-10-24 19:29:02 +0300

    schess

--- src/components/ImageContent/ImageContent.jsx
+++ src/components/ImageContent/ImageContent.jsx
@@ -11,7 +11,19 @@ const ImageContent = () => {
     return (
         <div className={styleCSS.imagecontent}>
             <img className={styleCSS.imgItem} src={image1} alt='imagecontent' />
-            <img className={styleCSS.imgItem} src={image2} alt='imagecontent' />
+            <br/>
+
+            <h2>Performance Testing:</h2>
+            <h3>Evaluate the software's performance under various workloads and stress conditions to identify
+                bottlenecks and optimize its speed and scalability.</h3>
+
+            <br/>
+            <h2>Usability Testing:</h2>
+            <h3>Evaluate the software's user interface (UI) and user experience (UX) to ensure it is intuitive,
+                easy
+                to
+                navigate, and meets the needs of the target audience.</h3>
+
             <img className={styleCSS.imgItem} src={image3} alt='imagecontent' />
             <img className={styleCSS.imgItem} src={image4} alt='imagecontent' />
         </div>
